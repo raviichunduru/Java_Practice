5e68b5570d2c546bbc2404b222c342642fceef76
admin <ravii.chunduru@gmail.com>
2024-04-19 16:28:15 +0530

    added Book.java

---
+++ src/main/java/POJO/Book.java
@@ -0,0 +1,26 @@
+package POJO;
+
+import lombok.Builder;
+
+public class Book implements Comparable<Book> {
+  String title;
+  String author;
+
+  public Book (String title, String author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  @Override
+  public String toString() {
+    return "Book{" +
+      "title='" + title + '\'' +
+      ", author='" + author + '\'' +
+      '}';
+  }
+
+  @Override
+  public int compareTo(Book Other) {
+    return this.title.compareTo(Other.title);
+  }
+}
